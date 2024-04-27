d374d1196b6f4f0808c9df663f6d873c7b6602e9
Kiminjo <deulseong1121@gmail.com>
2024-04-27 21:17:28 +0900

    [Test] ✅ Test docker server

--- test/server_test.py
+++ test/server_test.py
@@ -3,7 +3,7 @@ import os
 
 api_key = os.environ.get("OPENAI_API_KEY")
 db_path = "vector_store/"
-url = "http://127.0.0.1:8000/chat"
+url = "http://127.0.0.1:80/chat"
 
 output = requests.post(url, json={"api_key": api_key, 
                                   "db_path": db_path,
