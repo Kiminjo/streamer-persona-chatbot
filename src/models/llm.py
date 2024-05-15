f22de0a449433efce5cff7cd71afe7b40bb5b419
Kiminjo <deulseong1121@gmail.com>
2024-05-16 02:31:40 +0900

    "[Form] 💡 Add comment in llm.py code"

--- src/models/llm.py
+++ src/models/llm.py
@@ -11,6 +11,49 @@ from .vector_db import VectorDB
 from .configs import system_prompt_template
 
 class PersonaLLM:
+    """
+    Persona LLM class
+    Main LLM Module that generates responses based on the user query
+
+    Attributes:
+    ----------
+    llm_type: str
+        The type of the LLM model
+
+    model_name: str
+        The name of the LLM model
+
+    embedding_model_name: str
+        The name of the embedding model
+
+    api_key: str
+        The API key for the LLM model
+
+    db_path: str | Path
+        The path to the vector database
+
+    llm: ModelFactory
+        The LLM model
+
+    vector_db: VectorDB
+        The vector database
+
+    documents: List[str]
+        The list of documents
+
+    input_prompt: List[dict]
+        The list of input prompts
+
+
+    Examples:
+    ---------
+    >>> persona_llm = PersonaLLM(llm_type="openai",
+                                 model_name="gpt-3.5-turbo",
+                                 embedding_model_name="text-embedding",
+                                 api_key="api_key")
+    >>> persona_llm.search("query")
+    >>> persona_llm.generate("prompt")
+    """
     def __init__(self,
                  llm_type: str,
                  model_name: str,
@@ -32,14 +75,27 @@ class PersonaLLM:
 
         # Load the database
         self.vector_db = VectorDB(llm_type=llm_type,
-                                    embedding_model_name=embedding_model_name,
-                                    api_key=api_key)
+                                  embedding_model_name=embedding_model_name,
+                                  api_key=api_key)
         self.vector_db.load(db_path)
         self.documents = self.vector_db.documents
 
     def generate(self,
                  prompt: str,
                  ) -> str:
+        """
+        Generate the response based on the user prompt
+
+        Parameters:
+        -----------
+        prompt: str
+            The user prompt
+
+        Returns:
+        --------
+        ai_response: str
+            The generated response
+        """
         # Print warning if the prompt is empty
         if self.input_prompt == []:
             warnings.warn("System is empty. Please run 'search' method first.")
@@ -59,6 +115,15 @@ class PersonaLLM:
     def search(self,
                query: str
                ) -> None:
+        """
+        Search the database based on the user query
+        a.k.a RAG
+
+        Parameters:
+        -----------
+        query: str
+            The user query
+        """
         # Search the index
         idx = self.vector_db.search(query, k=1)
         related_doc = self.documents[idx[0]]
